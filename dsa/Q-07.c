33c47aadf85c00f720f5ee868b082faae10d6701
Shru Ku <1ms22cs126@msrit.edu>
2024-04-04 09:21:02 +0530

    Create Q-07.c

---
+++ dsa/Q-07.c
@@ -0,0 +1,72 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+#include <stdlib.h>
+
+#define size 6
+
+int curr = 0;
+
+typedef struct Node{
+    int value;
+    struct Node* next;
+} node;
+
+node* create(int key){
+    node* temp = (node*)malloc(sizeof(node));
+    temp->value = key;
+    temp->next = NULL;
+    
+    return temp;
+}
+
+node* insert(node* head, int key){
+    node* temp = create(key);
+    if(curr == size) return head;
+    curr++;
+    if(head == NULL) return temp;
+    
+    temp-> next = head;
+    return temp;
+}
+
+node* pop(node* head){
+    if(head == NULL) return NULL;
+    curr--;
+    if(head ->next == NULL) return NULL;
+    
+    node* temp = head-> next;
+    free(head);
+    return temp;
+}
+
+void display(node* head){
+    node* temp = head;
+    while(temp != NULL){
+        printf("%d ", temp->value);
+        temp = temp->next;
+    }
+    printf("\n");
+}
+
+
+int main() {
+   
+   node* head = NULL;
+   head = pop(head);
+   head = insert(head, 1);
+   head = insert(head, 2);
+   head = insert(head, 3);
+   head = insert(head, 4);
+   head = insert(head, 1);
+   head = insert(head, 2);
+   head = insert(head, 3);
+   head = insert(head, 4);
+   display(head);
+   head = pop(head);
+   display(head);
+
+
+
+
+    return 0;
+}
