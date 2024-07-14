1c334f19e07cd6498cb5ef97eb8faacf43336c93
Shru Ku <1ms22cs126@msrit.edu>
2024-07-14 16:44:15 +0530

    Update and rename 01-A.py to dijkstra.py

---
+++ DAA/dijkstra.py
@@ -0,0 +1,36 @@
+def dijkstra(V, edges, S):
+    # Create adjacency list
+    adj = [[] for i in range(V)]
+    for u, v, wt in edges:
+        adj[u].append((v, wt))
+        adj[v].append((u, wt))  # For undirected graph
+
+    dist = [float('inf')] * V
+    dist[S] = 0
+    unvisited = set(range(V))
+    
+    while unvisited:
+        u = min(unvisited, key=lambda vertex: dist[vertex])
+        
+        if dist[u] == float('inf'):
+            break
+        
+        unvisited.remove(u)
+        
+        for v, weight in adj[u]:
+            if v in unvisited:
+                if dist[u] + weight < dist[v]:
+                    dist[v] = dist[u] + weight
+    
+    return dist
+
+v, e = list(map(int, input("enter the no. of vertex and edges: ").split()))
+edges = []
+for i in range(e):
+    edge = list(map(int, input("enter edge (u, v, wt): ").split()))
+    edges.append(edge)
+
+source = 0
+dist = dijkstra(v, edges, source)
+    
+print(dist)
