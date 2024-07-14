c0cdf8a6e9124f65e29bf1270a66e3aea87318f0
Shru Ku <1ms22cs126@msrit.edu>
2024-07-14 23:52:37 +0530

    Delete DAA/00-makegraph.py

--- DAA/00-makegraph.py
+++
@@ -1,9 +0,0 @@
-graph = {}
-
-nodes = int(input("enter the total number nodes : "))
-for i in range(nodes):
-    key = int(input("enter the key : "))
-    value = set((input(f"enter the nodes connected to {key} : ")).split())
-    graph[f"{key}"] = value
-    
-print(graph)
