7d3a4b5fb864ba0e162370a60d66577c97c53029
Shru Ku <1ms22cs126@msrit.edu>
2024-08-07 15:11:01 +0530

    Update bellman.py

--- DAA/bellman.py
+++ DAA/bellman.py
@@ -1,26 +1,33 @@
-def bellman_ford(V, edges, S):
-    dist = [float('inf')] * V
-    dist[S] = 0
+def bellman_ford(vertices, src, graph):
+    V = len(vertices)
+    dist = {vertex: float('inf') for vertex in vertices}
+    dist[src] = 0
 
-    for i in range(V - 1):
-        for u, v, wt in edges:
-            if dist[u] != float('inf') and dist[u] + wt < dist[v]:
-                dist[v] = dist[u] + wt
+    for _ in range(V - 1):
+        for u, v, w in graph:
+            if dist[u] + w < dist[v]:
+                dist[v] = dist[u] + w
 
-    for u, v, wt in edges:
-        if dist[u] != float('inf') and dist[u] + wt < dist[v]:
-            print("-ve cycle.")
-            return [-1]
+    for u, v, w in graph:
+        if dist[u] + w < dist[v]:
+            print("Graph contains a negative-weight cycle")
+            return None
 
     return dist
 
-v, e = list(map(int, input("enter the no. of vertex and edges: ").split()))
-edges = []
-for i in range(e):
-    edge = list(map(int, input("enter edge (u, v, wt): ").split()))
-    edges.append(edge)
-    
-source = 0
-dist = bellman_ford(v, edges, source)
-    
-print(dist)
+vertices = ['a', 'b', 'c', 'd', 'e', 't']
+
+graph = [
+    ('a', 'b', -4),
+    ('a', 't', -3),
+    ('b', 'd', -1),
+    ('b', 'e', -2),
+    ('c', 'b', 8),
+    ('c', 't', 3),
+    ('d', 'a', 6),
+    ('d', 't', 4),
+    ('e', 'c', -3),
+    ('e', 't', 2)
+]
+
+print(bellman_ford(vertices, 'a', graph))
