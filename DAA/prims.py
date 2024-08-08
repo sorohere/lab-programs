f74d43ee803c81af139d3652713102d034341c6d
Shru Ku <1ms22cs126@msrit.edu>
2024-08-08 19:58:22 +0530

    Update prims.py

--- DAA/prims.py
+++ DAA/prims.py
@@ -1,44 +1,51 @@
-def prim(V, edges, start):
-    adj = [[] for _ in range(V)]
-    for u, v, wt in edges:
-        adj[u].append((v, wt))
-        adj[v].append((u, wt))
-    
-    min_edge = [float('inf')] * V
-    min_edge[start] = 0
-    in_mst = [False] * V
-    mst_cost = 0
-    
-    for _ in range(V):
-        min_weight = float('inf')
-        u = -1
-        for v in range(V):
-            if not in_mst[v] and min_edge[v] < min_weight:
-                min_weight = min_edge[v]
-                u = v
-        
-        if u == -1:
-            break
-        
-        in_mst[u] = True
-        mst_cost += min_weight
-        
-        for v, wt in adj[u]:
-            if not in_mst[v] and wt < min_edge[v]:
-                min_edge[v] = wt
-    
-    return mst_cost
+def primsAlgo(graph, source):
+    visited = []
+    edges = []
+    ans = []
+    mstCost = 0
 
-# Example usage
-V = 5  # Number of vertices
-edges = [
-    (0, 1, 5),
-    (1, 2, 20),
-    (1, 3, 10),
-    (3, 4, 5),
-    (2, 4, 1)
-]
-start_vertex = 0  # Starting vertex for Prim's algorithm
+    visited.append(source)
+    for vertex in graph[source]:
+        edges.append((source, vertex[0], vertex[1]))
 
-mst_cost = prim(V, edges, start_vertex)
-print("Minimum Cost of Spanning Tree:", mst_cost)
+    while edges:
+        u, v, w = findMin(edges)
+        if v not in visited:
+            visited.append(v)
+            ans.append((u, v, w))
+            mstCost += w
+
+            for vertex in graph[v]:
+                if vertex[0] not in visited:
+                    edges.append((v, vertex[0], vertex[1]))
+
+    return ans, mstCost
+
+def findMin(edges):
+    min_edge = edges[0]
+    for edge in edges:
+        if edge[2] < min_edge[2]:
+            min_edge = edge
+
+    edges.remove(min_edge)
+    return min_edge
+
+num = int(input("Enter the number of edges: "))
+graph = {}
+src = -1
+srcvalue = float('inf')
+for i in range(num):
+    u, v, w = input("Enter the (source, destination, weight): ").split()
+    if int(w) < srcvalue:
+        src = int(u)
+    if int(u) not in graph:
+        graph[int(u)] = []
+    if int(v) not in graph:
+        graph[int(v)] = []
+    
+    graph[int(u)].append((int(v), int(w)))
+    graph[int(v)].append((int(u), int(w)))
+
+mst, mstCost = primsAlgo(graph, src)
+print("MST: ", mst)
+print("MST Cost: ", mstCost)
