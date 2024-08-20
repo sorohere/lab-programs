96cad0d3f68a28aebd9c8013a4a5397703174081
Shru Ku <1ms22cs126@msrit.edu>
2024-08-20 18:35:48 +0530

    Update lateness.py

--- DAA/lateness.py
+++ DAA/lateness.py
@@ -6,14 +6,16 @@ list.sort(key = lambda x : x[1])
 
 while list:
   x = list.pop(0)
-  start = t
+  start = time
   end = start + x[0]
+
+  ans.append(start, end)
 
   if end - x[1] > 0:
     delay.append(end - x[1])
   else:
     delay.append(0)
 
-  ans.append(start, end)
+
   
   time = end
