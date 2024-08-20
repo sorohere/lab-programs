161a94b1ca41a16d3fb25de2ed3a36b4a85f3f47
Shru Ku <1ms22cs126@msrit.edu>
2024-08-20 18:33:03 +0530

    Create lateness.py

---
+++ DAA/lateness.py
@@ -0,0 +1,16 @@
+ans = []
+delay = []
+time = 0
+
+list
+
+while list:
+  x = list.pop(0)
+  start = t
+  end = start + x[0]
+
+  if end - x[1] > 0:
+    delay.append(end - x[1])
+  else:
+    delay.append(0)
+  time = end
