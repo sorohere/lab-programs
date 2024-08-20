52823afb5746febb39c1860bc45aaa005abfcf99
Shru Ku <1ms22cs126@msrit.edu>
2024-08-20 12:47:02 +0530

    Update interval.py

--- DAA/interval.py
+++ DAA/interval.py
@@ -1,39 +1,40 @@
-# def weighted_interval_scheduling(intervals):
-#     intervals.sort(key=lambda x: x[1])
+def weighted_interval_scheduling(intervals):
+    intervals.sort(key=lambda x: x[1])
 
-#     n = len(intervals)
-#     dp = [0] * n
+    n = len(intervals)
+    dp = [0] * n
 
-#     dp[0] = intervals[0][2] 
-#     for i in range(1, n):
-#         value_incl = intervals[i][2]
-#         j = find_latest_non_overlapping(intervals, i)
-#         if j != -1:
-#             value_incl += dp[j]
+    dp[0] = intervals[0][2] 
+    for i in range(1, n):
+        value_incl = intervals[i][2]
+        j = find_latest_non_overlapping(intervals, i)
+        if j != -1:
+            value_incl += dp[j]
 
-#         value_excl = dp[i-1]
+        value_excl = dp[i-1]
 
-#         dp[i] = max(value_incl, value_excl)
+        dp[i] = max(value_incl, value_excl)
 
-#     return dp[n-1]
+    return dp[n-1]
 
-# def find_latest_non_overlapping(intervals, current_index):
-#     for j in range(current_index - 1, -1, -1):
-#         if intervals[j][1] <= intervals[current_index][0]:
-#             return j
-#     return -1
+def find_latest_non_overlapping(intervals, current_index):
+    for j in range(current_index - 1, -1, -1):
+        if intervals[j][1] <= intervals[current_index][0]:
+            return j
+    return -1
 
 
-# n = int(input("Enter the number of intervals: "))
-# intervals = []
+n = int(input("Enter the number of intervals: "))
+intervals = []
 
-# for i in range(n):
-#     interval = list(map(int, input(f"enter (start, end, weight): ").split()))
-#     intervals.append(interval)
+for i in range(n):
+    interval = list(map(int, input(f"enter (start, end, weight): ").split()))
+    intervals.append(interval)
 
-# max_value = weighted_interval_scheduling(intervals)
-# print(f"Maximum value subset of intervals: {max_value}")
+max_value = weighted_interval_scheduling(intervals)
+print(f"Maximum value subset of intervals: {max_value}")
 
+print("***************************************************************************************************")
 
 def interval(edge, n):
     edge.sort(key = lambda x:x[1])
