5f06a1607fd0438f27523e442d358be20803afc1
soro <1ms22cs126@msrit.edu>
2025-03-18 11:17:58 +0530

    Create pg03.c

---
+++ USL/week01/pg03.c
@@ -0,0 +1,17 @@
+#include "apue.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+    int c;
+
+    while ((c = getc(stdin)) != EOF) {
+        if (putc(c, stdout) == EOF) {
+            printf("output error\n");
+        }
+    }
+
+    if (ferror(stdin)) printf("input error\n");
+
+    exit(0);
+}
