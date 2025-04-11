cca0dc6953ec3c22af7c34d5ce6b6d5d16498d94
soro <1ms22cs126@msrit.edu>
2025-04-11 10:16:06 +0530

    Create readme.sh

---
+++ devsecops/codeCoverage/readme.sh
@@ -0,0 +1,7 @@
+# helps you
+
+1. to install the req: `./installme.sh`
+2. have your cpp code ready in main.cpp
+3. run the code coverage: `./run.sh`
+
+to give any permission to the files use: `chmod +x path/to/the/file`
