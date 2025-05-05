e48982afa19408b516dadbab92094085f2f85dfd
soro <1ms22cs126@msrit.edu>
2025-05-05 21:41:08 +0530

    Update 01b.c

--- USL/exam-01/01b.c
+++ USL/exam-01/01b.c
@@ -9,15 +9,17 @@
 int main(int c, char **v) {
     int f = open(v[1], 0);
     char b[6];
+    
     pid_t pid = fork();
+    
     if (pid == 0) {
-        read(f, b, 5); 
+        read(f, &b, 5); 
         b[5] = 0;
         printf("Child: %s\n", b);
     } 
     else {
         wait(0);
-        read(f, b, 5); 
+        read(f, &b, 5); 
         b[5] = 0;
         printf("Parent: %s\n", b);
     }
